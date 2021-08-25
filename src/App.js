fa75aadb47b9772487b9bf1280e7570c4aa6aac7
nikethanrai <nikethanrai58@gmail.com>
2021-08-25 19:11:23 +0530

    css added

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import './App.css';
+
+function App() {
+  const [appdraw, setAppdraw] = useState(0)
+  return (
+    <div className="App">
+      <header className="header" id="header">
+        <nav className="nav container">
+          <a href="#" className="nav_logo">Rai</a>
+          <div className="nav_menu" id="nav-menu">
+            <ul className="nav_list grid">
+              <li className="nav_item">
+                <a href="#home" className="nav_link">
+                <i class="uil uil-estate nav_icon"></i>Home
+                </a>
+                
+
+              </li>
+              <li className="nav_item">
+                <a href="#about" className="nav_link">
+                <i class="uil uil-user nav_icon"></i>About
+                </a>
+
+              </li>
+              <li className="nav_item">
+                <a href="#skills" className="nav_link">
+                <i class="uil uil-analytics nav_icon"></i>Skills
+                  
+                </a>
+
+              </li>
+              <li className="nav_item">
+                <a href="#projects" className="nav_link">
+                <i class="uil uil-arrow nav_icon"></i>Projects
+                </a>
+
+              </li>
+              <li className="nav_item">
+                <a href="#contact" className="nav_link">
+                <i class="uil uil-message nav_icon"></i>ContactMe
+                </a>
+
+              </li>
+
+            </ul>
+            <i class="uil uil-times nav_close" id='nav-close'></i>
+
+          </div>
+          <div className="nav_btns">
+            <div className="nav_toggle" id="nav-toggle">
+            <i class="uil uil-apps"></i>
+
+            </div>
+          </div>
+
+        </nav>
+
+      </header>
+     
+      
+     
+    </div>
+  );
+}
+
+export default App;
